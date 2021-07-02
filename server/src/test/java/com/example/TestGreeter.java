fa59954630977ea2685fbdec7a8777b4e90e4d7c
fguyont <47558498+fguyont@users.noreply.github.com>
2021-07-02 11:13:39 +0200

    failed tests
    faire échouer les tests

--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -20,14 +20,18 @@ public class TestGreeter {
   @Test
   public void greetShouldIncludeTheOneBeingGreeted() {
     String someone = "World";
+    String someone2 = "Not the World";
 
-    assertThat(greeter.greet(someone), containsString(someone));
+    assertThat(greeter.greet(someone), containsString(someone2));
+    //assertThat(greeter.greet(someone), containsString(someone));
   }
 
   @Test
   public void greetShouldIncludeGreetingPhrase() {
     String someone = "World";
+    String someone2 = "Not the World";
 
-    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone2.length())));
+    //assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
 }
